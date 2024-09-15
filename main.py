94c5215985b526e97487c0cf0763fc79c840333f
NooBzy <mohanad.aldaghestani@gmail.com>
2024-09-15 20:18:14 +0200

    Added list of dictionary

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_char = get_num_char(text)
-    print(text)
-    print(num_words)
+    char_list_of_dict = get_list_dict(num_char)
+
+
+    #num_char_sorted = sort_char_count(num_char)
+
+
+
+    print (f"=== Begin report of {book_path} ===")
+    # print(text)
+    print(f"{num_words} words found in the document")
+    print("Character appearance in the document:")
     print(num_char)
+    print (char_list_of_dict)
+    print("=== End report ===")
 
 def get_book_text(path):
     with open(path) as f:
@@ -28,6 +39,10 @@ def get_num_char(text):
         else:
             char_count[char] += 1
     return char_count
+
+def get_list_dict(dict):
+    list_of_dict = [{key:value} for key, value in dict.items()}
+    return list_of_dict
 
 
 
