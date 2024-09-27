7724bc1f2f2bee8d89dab8e9435fa1fc0341a80f
NooBzy <mohanad.aldaghestani@gmail.com>
2024-09-27 12:34:33 +0200

    Changeed book_path and addded sorting fuction

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-# from collections import Counter
+import os
+
 
 def main():
-    # Reads the content of the book and stores it as a value
-    book_path = "books/frankenstein.txt"
+# Start up fuctions at the begining of the program
+    # current_directory = os.getcwd()
+    # print(f"Current Directory: {current_directory}")
+
+    book_path = "/home/noobzy/workspace/github.com/mohald-3/BookBot/books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_char = get_num_char(text)
-    char_list_of_dict = get_list_dict(num_char)
+    list_of_dict = get_list_dict(num_char)
+    list_of_dict_sorted = sorted(list_of_dict, reverse = True, key = sort_on)
 
+# End up fuctions at the begining of the program
 
-    #num_char_sorted = sort_char_count(num_char)
-
-
+# Start Console Text
 
     print (f"=== Begin report of {book_path} ===")
     # print(text)
     print(f"{num_words} words found in the document")
     print("Character appearance in the document:")
-    print(num_char)
-    print (char_list_of_dict)
+    # print(num_char)
+    # print(list_of_dict)
+    # print(list_of_dict_sorted)
+    print_sorted_list(list_of_dict_sorted)
+
     print("=== End report ===")
+
+# End Console Text
+
+# Fuctions start
 
 def get_book_text(path):
     with open(path) as f:
@@ -28,9 +39,9 @@ def get_book_text(path):
 def get_num_words(text):
     words = text.split()
     return len(words)
-    # returns the word count of the book
+# returns the word count of the book
 
-    # fuction that returns a dictionary of unique character and its occurrence in the book 
+# fuction that returns a dictionary of unique character and its occurrence in the book 
 def get_num_char(text):
     char_count = {}
     for char in text.lower():
@@ -40,12 +51,19 @@ def get_num_char(text):
             char_count[char] += 1
     return char_count
 
-def get_list_dict(dict):
-    list_of_dict = [{key:value} for key, value in dict.items()}
-    return list_of_dict
+# assigns the letter to its occurrence count
+def get_list_dict(num_char):
+    list_of_dicts_char = [{"character": key, "count": value} for key, value in num_char.items()]
+    return list_of_dicts_char
 
+# prints out a list of char and its value sorted accendedly by the occurence count
+def sort_on(dict):
+    return dict["count"]
 
-
+def print_sorted_list(list):
+    for item in list:
+        if item["character"].isalpha():
+            print(f'The letter {item["character"]} appeared {item["count"]} times in the book') 
 
     
 if __name__ == "__main__":
